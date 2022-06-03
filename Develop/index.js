5bea5189b2f7eb659485c2c24c4628f60c472f65
JPage96 <justice.page1@pepsico.com>
2022-06-02 23:35:39 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,49 @@ const fs = require('fs');
 const inquirer = require('inquirer'); 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub username.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Enter your GitHub username.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Enter your email address.');
+                    return false;
+                }    
+            }
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please enter your project name.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Enter your GitHub username.');
+                    return false;
+                }    
+            }
+        },
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
